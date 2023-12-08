8d72db4fb794d2b30817cf61c1380a5dec27c6ad
Pli <4444175+Plixs@users.noreply.github.com>
2023-12-08 18:54:03 +0800

    :alien:

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,3 +12,11 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+
+echo "not update go version" 
+sudo go version
+git clone https://github.com/udhos/update-golang
+sudo ./update-golang/update-golang.sh
+echo "updated go version" 
+source /etc/profile.d/golang_path.sh
+sudo go version
