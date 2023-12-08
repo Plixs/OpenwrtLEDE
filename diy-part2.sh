d11b80d7dbfedd5aee891d6918f32a35b2fec823
Pli <4444175+Plixs@users.noreply.github.com>
2023-12-08 19:20:20 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -13,11 +13,16 @@
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 
-sudo -s
+# sudo -s
+# echo "not update go version" 
+# sudo go version
+# git clone https://github.com/udhos/update-golang
+# sudo ./update-golang/update-golang.sh
+# echo "updated go version" 
+# source /etc/profile.d/golang_path.sh
+# sudo go version
+
 echo "not update go version" 
 sudo go version
-git clone https://github.com/udhos/update-golang
-sudo ./update-golang/update-golang.sh
-echo "updated go version" 
-source /etc/profile.d/golang_path.sh
+sudo snap install go --classic
 sudo go version
